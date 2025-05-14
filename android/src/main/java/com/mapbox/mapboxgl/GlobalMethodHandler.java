adc1db451eb8213bb24289f7e8601f49c83a2983
Abhishek <abhishek.thakur@jungleworks.com>
2025-05-14 16:45:49 +0530

    method channel fixes

--- android/src/main/java/com/mapbox/mapboxgl/GlobalMethodHandler.java
+++ android/src/main/java/com/mapbox/mapboxgl/GlobalMethodHandler.java
@@ -9,7 +9,7 @@ import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.PluginRegistry;
+//import io.flutter.plugin.common.PluginRegistry;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
@@ -26,14 +26,14 @@ class GlobalMethodHandler implements MethodChannel.MethodCallHandler {
   private static final int BUFFER_SIZE = 1024 * 2;
   @NonNull private final Context context;
   @NonNull private final BinaryMessenger messenger;
-  @Nullable private PluginRegistry.Registrar registrar;
+//  @Nullable private PluginRegistry.Registrar registrar;
   @Nullable private FlutterPlugin.FlutterAssets flutterAssets;
 
-  GlobalMethodHandler(@NonNull PluginRegistry.Registrar registrar) {
-    this.registrar = registrar;
-    this.context = registrar.activeContext();
-    this.messenger = registrar.messenger();
-  }
+//  GlobalMethodHandler(@NonNull PluginRegistry.Registrar registrar) {
+//    this.registrar = registrar;
+//    this.context = registrar.activeContext();
+//    this.messenger = registrar.messenger();
+//  }
 
   GlobalMethodHandler(@NonNull FlutterPlugin.FlutterPluginBinding binding) {
     this.context = binding.getApplicationContext();
@@ -142,9 +142,9 @@ class GlobalMethodHandler implements MethodChannel.MethodCallHandler {
       return new FileInputStream(new File(tilesDb));
     } else {
       String assetKey;
-      if (registrar != null) {
+      /*if (registrar != null) {
         assetKey = registrar.lookupKeyForAsset(tilesDb);
-      } else if (flutterAssets != null) {
+      } else*/ if (flutterAssets != null) {
         assetKey = flutterAssets.getAssetFilePathByName(tilesDb);
       } else {
         throw new IllegalStateException();
